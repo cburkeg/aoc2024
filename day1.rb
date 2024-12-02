49043bf44f9452b4cec5a9e5483abd73df4b1717
Conor Burke-Govey <cburkeg@gmail.com>
2024-12-02 23:32:15 +1300

    Day 1 Part 1

--- day1.rb
+++ day1.rb
@@ -1,1 +1,17 @@
-puts 'Hello, world!'
+require 'matrix'
+
+input_file = File.new "day1_input.text"
+puzzle_input = input_file.read
+
+left_list = []
+right_list = []
+
+puzzle_input.each_line do |line|
+  left_list.append(line.split[0].to_i)
+  right_list.append(line.split[1].to_i)
+end
+
+distance_vector = Vector.elements(left_list.sort) - Vector.elements(right_list.sort)
+summed_distance = distance_vector.to_a.reduce{|sum, n| (sum).abs + (n).abs}
+
+puts "The answer is: #{summed_distance.to_s}"
