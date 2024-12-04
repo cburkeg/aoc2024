29f95659ac40aee100ae359922a62c8df08c3ee8
Conor Burke-Govey <cburkeg@gmail.com>
2024-12-05 00:05:44 +1300

    Day 3 Part 2

---
+++ day3_part2.rb
@@ -0,0 +1,22 @@
+# Day 3 Part 2
+
+def get_total_of_do_block(do_block)
+  block_total = 0
+  do_block.scan(/[m][u][l][(]\d{1,3}[,]\d{1,3}[)]/).each do |mul_instruction|
+    puts "Mul instruction is #{mul_instruction}"
+    block_total += mul_instruction.scan(/\d{1,3}/).first.to_i * mul_instruction.scan(/\d{1,3}/).last.to_i 
+  end
+  block_total
+end
+
+input_file = File.new "day3_input.text"
+puzzle_input = input_file.read
+puzzle_input.gsub!("\n", "-")
+
+total = 0
+
+puzzle_input.scan(/(:?do\(\)|\A)(.*?)(:?don't\(\)|\z)/).each do |do_block|
+    total += get_total_of_do_block(do_block[1])
+  end
+
+puts "The total output is #{total}"
