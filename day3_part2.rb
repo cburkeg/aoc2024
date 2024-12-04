77ddd7e2a9593208203790c321e4a57ce8f0b251
Conor Burke-Govey <cburkeg@gmail.com>
2024-12-05 00:07:38 +1300

    Remove debug put

--- day3_part2.rb
+++ day3_part2.rb
@@ -3,7 +3,6 @@
 def get_total_of_do_block(do_block)
   block_total = 0
   do_block.scan(/[m][u][l][(]\d{1,3}[,]\d{1,3}[)]/).each do |mul_instruction|
-    puts "Mul instruction is #{mul_instruction}"
     block_total += mul_instruction.scan(/\d{1,3}/).first.to_i * mul_instruction.scan(/\d{1,3}/).last.to_i 
   end
   block_total
